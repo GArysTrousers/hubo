4cc1b96723b2513a2b812767430cedc863748e43
GArysTrousers <43531407+GArysTrousers@users.noreply.github.com>
2020-09-24 11:27:23 +1000

    Update hubo.js

--- assets/hubo.js
+++ assets/hubo.js
@@ -1,29 +1,3 @@
-var settings = {
-  "apps_in_row": 4,
-  "apps": [
-    {
-      "name": "Emby",
-      "link": "http://sv01:8096",
-      "icon": "emby.png"
-    },
-    {
-      "name": "Netflix",
-      "link": "http://www.netflix.com/WiHome",
-      "icon": "netflix.png"
-    },
-    {
-      "name": "Stan",
-      "link": "https://www.stan.com.au/",
-      "icon": "stan.png"
-    },
-    {
-      "name": "YouTube",
-      "link": "https://www.youtube.com/",
-      "icon": "youtube.png"
-    }
-  ]
-}
-
 function AddApps() {
   let col_width = 12 / settings.apps_in_row;
   let html = "";
